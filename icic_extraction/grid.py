abe5856fde4a23d3c3d7029e56ed5ed577e9a146
mostafakamel <mostafazeiada3@gmail.com>
2022-09-27 21:42:00 +0200

    icic code after comments

--- icic_extraction/grid.py
+++ icic_extraction/grid.py
@@ -8,6 +8,10 @@ def get_text_from_pdf_with_plumber(apdf):
             tot += "\n"+p.extract_text()
     return tot 
 def get_meta_from_contract(contract_cont):
+    """
+    This function to get meta data for contract
+    and it return alist of basic inf for contract
+    """
     contract_note_no = re.findall("Contract Note No.*?:(.*)",contract_cont)[0]
     trade_date = re.findall ("TRADE DATE\s*?:\s*(\d{2}-\d{2}-\d{4}).*",contract_cont)[0]
     settlemnt_no = re.findall ("SETTLEMENT NO.*:\s*(\d+).*",contract_cont)[0]
@@ -17,13 +21,17 @@ def get_meta_from_contract(contract_cont):
     pan_of_client = re.findall ("PAN of Client\s*:\s*(.*)",contract_cont)[0]
     meta_contract = [contract_note_no,trade_date,settlemnt_no,settlement_date,mob_num,ucc_no,pan_of_client]
     return meta_contract
-def get_summary_table(contract_cont):
+def get_summary_table_list(contract_cont):
     seperate_summary_table = re.findall("Transaction Tax(.*?)\s*Brokerage Charges Charges.*?Buy(.*?)Sell(.*?)Total Payable / Receivable(.*?)\n",contract_cont,re.DOTALL)
     return seperate_summary_table
-def get_detailed_table (contract_cont):
+def get_detailed_table_list (contract_cont):
     seperate_detailed_table = re. findall("\d+\s+\d+\s+\d+\s+[\d{2}:\d{2}].*",contract_cont)
     return seperate_detailed_table
 def extract_transaction_from_detailed_table(detailed_table):
+    """
+    Input is detailed tables list 
+    and it return  list of detailed tables data
+    """
     transactions=[]   
     for record in detailed_table :
         records_list = record.split()
@@ -33,6 +41,10 @@ def extract_transaction_from_detailed_table(detailed_table):
         transactions.append(records_list)
     return transactions
 def extract_transaction_from_summary_table (summary_table):
+    """
+    input is summary table list 
+    and it returns list of summary table data
+    """
     summary_table_data = []
     for transaction  in summary_table:
         clean_transaction = tuple(element.strip() for element in transaction)
@@ -42,7 +54,7 @@ def extract_transaction_from_summary_table (summary_table):
         transaction_record.insert(0,company_name)
         summary_table_data.append(transaction_record)
     return summary_table_data
-def get_meta_table(contract_cont):
+def get_totals_table(contract_cont):
     pay_in_obligtion =re.findall("Pay in Obligation\s+.*(\d{5}\.\d+)",contract_cont)[0]
     exchange_trans_charges = re.findall(" Exchange Transaction Charges.*(\d+\.\d+)",contract_cont)[0]
     sebi_fees = re.findall("SEBI Turnover Fees.*(\d+\.\d+)",contract_cont)[0]
@@ -60,25 +72,26 @@ def extract_meta_summary_table(summary_table):
     total_meta_summary = re.findall("TOTAL.*\s+(\d+\.\d+).*",summary_table)[0]
     securties_meta_summary = re.findall("Securities Transaction Tax.*\s+(\d+\.\d+).*",summary_table)[0]
     return total_meta_summary,securties_meta_summary
-def extract_transaction_summary_table (summary_table):
+def extract_transaction_final_summary_table (summary_table):
     transacton_summary_table = re.findall(".*\d+\s\d+\.\d+\s+\d+\.\d+",summary_table)
     records = [i.split() for i in transacton_summary_table]
     return records
 def extract_final_summary_table(summary_table):
     meta_summary = extract_meta_summary_table(summary_table)
-    transaction_summary = extract_transaction_summary_table(summary_table)
+    transaction_summary = extract_transaction_final_summary_table(summary_table)
     return (transaction_summary,meta_summary)
 def get_data_from_contract (contract_path):
     f_content = get_text_from_pdf_with_plumber(contract_path)
     meta_contract = get_meta_from_contract(f_content)
-    summary_table_extraction = get_summary_table(f_content)
-    detailed_table_extraction = get_detailed_table(f_content)
+    summary_table_extraction = get_summary_table_list(f_content)
+    detailed_table_extraction = get_detailed_table_list(f_content)
     transaction_detailed_table = extract_transaction_from_detailed_table(detailed_table_extraction)
-    transaction_summary_table = extract_transaction_summary_table(summary_table_extraction)
-    extra_meta = get_meta_table(f_content)
+    transaction_summary_table = extract_transaction_from_summary_table(summary_table_extraction)
+    extra_meta = get_totals_table(f_content)
     final_summary_table = seperate_final_summary_table(f_content)
     final_summary_res = extract_final_summary_table(final_summary_table)
     final_res = [meta_contract,transaction_detailed_table,transaction_summary_table,extra_meta,final_summary_res]
     return final_res
-
-
+if __name__ == "__main__" : 
+    res = get_data_from_contract("icic.pdf")
+    print (res)
