b0a43fd2b26ab5202c9572a5ed74511b9e46c27a
Mostafa Zeiada <mostafazeiada@Mostafas-MacBook-Pro.local>
2022-10-06 19:43:03 +0200

    icic extraction version 2

--- icic_extraction/grid.py
+++ icic_extraction/grid.py
@@ -1,8 +1,11 @@
 import re
+from tkinter import Y
 from xml.dom.minidom import Element 
 import pandas as pd
 import pdfplumber
 import glob 
+from difflib import SequenceMatcher 
+import pandas as pd
 
 def get_text_from_pdf_with_plumber(apdf):
     tot = ''
@@ -10,6 +13,7 @@ def get_text_from_pdf_with_plumber(apdf):
         for p in pdf.pages:
             tot += "\n"+p.extract_text()
     return tot 
+x =get_text_from_pdf_with_plumber("/Users/mostafazeiada/Desktop/mostafa/gk_intern_0/icic_extraction/icici_cont/12272021.pdf")
 def get_meta_from_contract(contract_cont):
     """
     This function to get meta data for contract
@@ -24,8 +28,11 @@ def get_meta_from_contract(contract_cont):
     meta_contract = [contract_note_no,trade_date,settlement_date,mob_num,ucc_no,pan_of_client]
     return meta_contract
 def get_summary_table_list(contract_cont):
-    seperate_summary_table = (re.findall("Transaction Tax.*\n(.*?)\s*Service Tax.*?Buy(.*?)Sell(.*?)Total Payable / Receivable(.*?)\n",contract_cont,re.DOTALL))
+    seperate_summary_table= re.findall("(.*)Service Tax.*\n(Buy.*)\n(Sell.*)?\n",contract_cont,)
+    #buy = "Sell  0 0.00 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000"
+    #sell = "Sell  0 0.00 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000 0.0000"
     return seperate_summary_table
+
 def get_detailed_table_list (contract_cont):
     seperate_detailed_table = re. findall("\d{2}:\d{2}.*(?:Buy|Sell).*",contract_cont)
     return seperate_detailed_table
@@ -63,6 +70,19 @@ def extract_transaction_from_summary_table (summary_table):
         transaction_record.insert(0,company_name)
         summary_table_data.append(transaction_record)
     return summary_table_data
+
+def clean_trans_summary(sum_update):
+    res = []
+    for i in sum_update:
+        if i[1][1]!= "0":
+            res.append(i[1]+[i[0],i[-1]])
+        if i[2][1] != "0":
+            res.append(i[2]+[i[0],i[-1]])
+    return res
+
+def flatten_transaction_table (meta,clean_trans):
+    return [i+meta for i in clean_trans]
+
 def get_totals_table(contract_cont):
     pay_in_obligtion = re.findall("Pay\s.*(?:in|out) Obligation\s+.*?(\d+\.\d+)",contract_cont)[0]
     if len (re.findall("Exchange Transaction .*Charges.*?(\d+\.\d+)",contract_cont)) == 0 :
@@ -111,22 +131,77 @@ def extract_final_summary_table(summary_table):
     meta_summary = extract_meta_summary_table(summary_table)
     transaction_summary = extract_transaction_final_summary_table(summary_table)
     return (transaction_summary,meta_summary)
+
+def transform_detailed_table_to_name_isin_dict(tb):
+    adict = {}
+    for i in tb:
+        adict[i[2]] = i[1]
+    return adict 
+
+def map_src_to_dst(res):
+    src = []
+    dest = []
+    adict = {}
+    for i in res:
+        if not(i[1] in src) and not(i[2] in dest):
+            src.append(i[1])
+            dest.append(i[2])
+            adict[i[1]] = i[2]
+    return adict
+def generate_match_percentage(summ_names,trans_names):
+    res = []
+    for i in summ_names : 
+        for j in trans_names : 
+            match0 = 100*round(SequenceMatcher(None, i.lower(), j.lower()).ratio(),2)
+            res.append([match0,i,j])
+    res.sort(reverse=True)
+    return res 
+
+def get_detailed_name(s,d):
+    res = generate_match_percentage(s,d)
+    adict = map_src_to_dst(res)
+    return adict
+
+def update_summary_with_fetched_isin(st,ddict):
+    for i in st :
+        name = i[0].strip()
+        try:
+            isin = ddict[name]
+        except:
+            res = get_detailed_name([name],ddict.keys())
+            isin = ddict[res[name]]
+        i.append(isin)
+    return st
+
 def get_data_from_contract (contract_path):
     f_content = get_text_from_pdf_with_plumber(contract_path)
     meta_contract = get_meta_from_contract(f_content)
     summary_table_extraction = get_summary_table_list(f_content)
     detailed_table_extraction = get_detailed_table_list(f_content)
     transaction_detailed_table = extract_transaction_from_detailed_table(detailed_table_extraction)
+    trans_table_dict = transform_detailed_table_to_name_isin_dict(transaction_detailed_table)
     transaction_summary_table = extract_transaction_from_summary_table(summary_table_extraction)
-    extra_meta = get_totals_table(f_content)
-    final_summary_table = seperate_final_summary_table(f_content)
-    final_summary_res = extract_final_summary_table(final_summary_table)
-    final_res = [meta_contract,transaction_detailed_table,transaction_summary_table,extra_meta,final_summary_res]
-    return final_res
+    summary_table_updated = update_summary_with_fetched_isin(transaction_summary_table,trans_table_dict)
+    clean_summ = clean_trans_summary(summary_table_updated)
+    flatten_trans = flatten_transaction_table(meta_contract,clean_summ)
+    contract_df = pd.DataFrame(flatten_trans)
+    cols = ['Trans_Type','Quantity','Gross_Value','Total_Brokerage','Exchange_Trans_Charges','SEBI','Total_GST','Stamp_Duty','STT','Net_Payable_Recievable']
+    cols += ['Company','ISIN','ContracT_Note_No','Trade_Date','Settlement_Date','Mobile_No','UCC','PAN']
+    contract_df.columns = cols
+    return contract_df
+
 if __name__ == "__main__" : 
     files = glob.glob('icici_cont/*.pdf')
-    for i in files :
+    tot = pd.DataFrame()
+    for i in files:
         print("Contract: ", i )
-        res = get_data_from_contract(i)
-        print(*res,sep='\n')
+        try:
+            res = get_data_from_contract(i)
+            res['file'] = "/Users/mostafazeiada/Desktop/mostafa/gk_intern_0/icic_extraction/icici_cont/" + i
+            tot = pd.concat([tot,res])
+        except:
+            print("FAILED ON : ", i )
         print("="*30)
+    tot.to_csv("ici_x.csv",index=False)
+
+
